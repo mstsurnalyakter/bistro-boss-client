1e1abee1607499018c6a105a3076d0bd15408238
mstsurnalyakter <surnalyms@gmail.com>
2024-05-17 02:32:59 +0600

    json data added

--- src/Pages/Home/Category/Category.jsx
+++ src/Pages/Home/Category/Category.jsx
@@ -8,50 +8,54 @@ import carousel2 from '../../../assets/home/slide2.jpg'
 import carousel3 from '../../../assets/home/slide3.jpg'
 import carousel4 from '../../../assets/home/slide4.jpg'
 import carousel5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from "../../components/SectionTitle/SectionTitle";
 
 const Category = () => {
   return (
-    <Swiper
-      slidesPerView={4}
-      spaceBetween={30}
-      centeredSlides={true}
-      pagination={{
-        clickable: true,
-      }}
-      modules={[Pagination]}
-      className="mySwiper mb-24"
-    >
-      <SwiperSlide>
-        <img src={carousel1} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-          Salads
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={carousel2} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-          Pizza
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={carousel3} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-          Soups
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={carousel4} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-          Desserts
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={carousel5} alt="" />
-        <h3 className="text-4xl uppercase text-center -mt-16 text-white">
-          Salads
-        </h3>
-      </SwiperSlide>
-    </Swiper>
+    <section>
+      <SectionTitle heading="Order Online" subHeading='From 11.00am to 10.pm' />
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={30}
+        centeredSlides={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper mb-24"
+      >
+        <SwiperSlide>
+          <img src={carousel1} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={carousel2} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={carousel3} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={carousel4} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Desserts
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={carousel5} alt="" />
+          <h3 className="text-4xl uppercase text-center -mt-16 text-white">
+            Salads
+          </h3>
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../components/SectionTitle/SectionTitle"
+
+
+const PopularMenu = () => {
+  return (
+    <section>
+      <SectionTitle heading="From Our Menu" subHeading="Popular Items" />
+    </section>
+  );
+}
+
+export default PopularMenu
