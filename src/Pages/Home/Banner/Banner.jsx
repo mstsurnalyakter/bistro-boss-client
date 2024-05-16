5591eba10747ee3d95de193f59c9e96e85a6ec8c
mstsurnalyakter <surnalyms@gmail.com>
2024-05-16 23:46:42 +0600

    project basic setup

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,26 @@
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import img1 from '../../../'
+
+const Banner = () => {
+  return (
+    <div>
+      <Carousel>
+        <div>
+          <img src="assets/1.jpeg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="assets/2.jpeg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Banner;
