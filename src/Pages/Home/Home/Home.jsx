0a8821ab50475106bebd2f9b456bfc722202899f
mstsurnalyakter <surnalyms@gmail.com>
2024-05-17 04:16:23 +0600

    Testimonials section added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -2,6 +2,7 @@ import Banner from "../Banner/Banner"
 import Category from "../Category/Category"
 import Feature from "../Feature/Feature"
 import PopularMenu from "../PopularMenu/PopularMenu"
+import Testimonials from "../Testimonials/Testimonials"
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <Category/>
       <PopularMenu/>
       <Feature/>
+      <Testimonials/>
     </div>
   )
 }
--- src/Pages/Home/Testimonials/Testimonials.jsx
+++ src/Pages/Home/Testimonials/Testimonials.jsx
@@ -1,9 +1,57 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../components/SectionTitle/SectionTitle"
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 
+
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
 
 const Testimonials = () => {
+    const [reviews,setReviews] = useState();
+    useEffect(()=>{
+        fetch('reviews.json')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+        .catch(error=>console.error(error))
+    },[])
+    console.log(reviews);
   return (
-    <div>Testimonials</div>
-  )
+    <section className="my-20">
+      <SectionTitle heading="Testimonials" subHeading="What Our Client Say" />
+      <div>
+        <Swiper
+          pagination={{
+            type: "fraction",
+          }}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation,Autoplay]}
+          className="mySwiper"
+        >
+          {reviews?.map((review) => (
+            <SwiperSlide key={review?._id}>
+              <div className="flex flex-col items-center py-16  mx-24">
+                <Rating
+                  style={{ maxWidth: 180 }}
+                  value={review?.rating}
+                  readOnly
+                />
+                <p className="py-8">{review?.details}</p>
+                <h3 className="text-2xl text-orange-400">{review?.name}</h3>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 }
 
 export default Testimonials
