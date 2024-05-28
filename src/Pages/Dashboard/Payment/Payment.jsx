196d15cc5371010cca9f09feff37a970b4bb8977
mstsurnalyakter <surnalyms@gmail.com>
2024-05-28 21:31:20 +0600

    payment system basic setup

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import SectionTitle from '../../../components/SectionTitle/SectionTitle'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import CheckoutForm from './CheckoutForm'
+
+
+//TODO:add publishable key
+const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
   return (
     <div>
         <SectionTitle heading='Payment' subHeading='Please pay to eat' />
         <div>
-            <h2 className='text-4xl'>Taka o pakhi tumi uira uira aso</h2>
+           <Elements stripe={stripePromise} >
+              <CheckoutForm/>
+           </Elements>
         </div>
     </div>
   )
