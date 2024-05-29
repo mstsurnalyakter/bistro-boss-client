5802b7616753c300eaed49c8131a51d950cddeca
mstsurnalyakter <surnalyms@gmail.com>
2024-05-29 15:47:09 +0600

    payment functionality added

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -3,6 +3,7 @@ import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import useAxiosSecure from '../../../hooks/useAxiosSecure';
 import useCart from '../../../hooks/useCart';
 import useAuth from '../../../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 const CheckoutForm = () => {
 
@@ -12,7 +13,7 @@ const CheckoutForm = () => {
     const stripe = useStripe();
     const elements = useElements();
     const axiosSecure = useAxiosSecure();
-    const [cart] = useCart();
+    const [cart, refetch] = useCart();
     const {user} = useAuth();
     const totalPrice = cart.reduce((total,item)=>total+item?.price,0)
 
@@ -28,10 +29,6 @@ const CheckoutForm = () => {
            });
        }
      }, [axiosSecure, totalPrice]);
-
-
-
-
 
 
     const handleSubmit = async e =>{
@@ -78,11 +75,38 @@ const CheckoutForm = () => {
           if (paymentIntent.status === 'succeeded') {
             console.log('transaction id',paymentIntent.id);
             setTransactionId(paymentIntent?.id)
+
+            //now save the payment in the database
+            const payment = {
+              email: user?.email,
+              price: totalPrice,
+              transactionId:paymentIntent.id,
+              date: new Date(), //utc date convert. use moment js to
+              cardIds: cart?.map((item) => item?._id),
+              menuItemIds: cart.map((item) => item?.menuId),
+              status:'pending'
+            };
+
+            const {data} = await axiosSecure.post("/payments",payment);
+            console.log("payment saved",data);
+            refetch();
+            if (data?.paymentResult?.insertedId) {
+              Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Payment successfully.",
+                showConfirmButton: false,
+                timer: 1500,
+              });
+            }
+
           }
         }
 
 
     }
+
+
   return (
     <form onSubmit={handleSubmit}>
       <CardElement
